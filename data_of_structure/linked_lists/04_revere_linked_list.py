aea5bd78ae6ce24c55418e8a4b7027c0af210512
Souza <lucas11souza97@gmail.com>
2025-12-26 18:53:42 -0400

    feat: added reverse in linked list

---
+++ data_of_structure/linked_lists/04_revere_linked_list.py
@@ -0,0 +1,109 @@
+class Node:
+    def __init__(self, value: int):
+        self.value = value
+        self.next: Node | None = None
+
+
+class LinkedList:
+    def __init__(self, value: int):
+        node = Node(value)
+        self.head: Node = node
+        self.tail: Node = self.head
+        self.length = 1
+
+    def append(self, value: int):
+        node = Node(value)
+        self.tail.next = node
+        self.tail = node
+        self.length += 1
+
+    def preppend(self, value: int):
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+        self.length += 1
+
+    def printList(self):
+        lists = []
+        currentNode = self.head
+        while currentNode != None:
+            lists.append(currentNode.value)
+            currentNode = currentNode.next
+        print(lists)
+
+    def getByValue(self, value: int):
+        currentNode = self.head
+        while currentNode != None:
+            if currentNode.value == value:
+                break
+            currentNode = currentNode.next
+
+    def getByIndex(self, index: int):
+        countIndex = 0
+        currentNode = self.head
+        while currentNode != None:
+            if countIndex == index:
+                break
+            currentNode = currentNode.next
+            countIndex += 1
+        return currentNode
+
+    def insert(self, index: int, value: int):
+        if index >= self.length:
+            self.append(value)
+            return
+
+        if index == 0:
+            self.preppend(value)
+            return
+
+        newNode = Node(value)
+        leaderWithPassNode = self.getByIndex(index - 1)
+
+        newNode.next = leaderWithPassNode.next
+        leaderWithPassNode.next = newNode
+
+        self.length += 1
+
+    def remove(self, index: int):
+        if index == 0:
+            firstNode = self.head
+            self.head = firstNode.next
+            self.length -= 1
+            return
+        leaderWithPassNode = self.getByIndex(index - 1)
+        if leaderWithPassNode.next == None:
+            print("Este index não existe")
+            return
+        else:
+            removedNode = leaderWithPassNode.next
+            if removedNode.next == None:
+                self.tail = leaderWithPassNode
+            leaderWithPassNode.next = removedNode.next
+            self.length -= 1
+
+    def reverse(self):
+        if self.head.next is None:
+            return
+        first = self.head
+        second = first.next
+        while second != None:
+            temp = second.next
+            second.next = first
+            first = second
+            second = temp
+
+        self.head.next = None
+        self.head = first
+
+
+linkedList = LinkedList(value=10)
+linkedList.append(18)
+linkedList.append(1)
+linkedList.append(16)
+linkedList.preppend(17)
+print()
+
+linkedList.printList()
+linkedList.reverse()
+linkedList.printList()
